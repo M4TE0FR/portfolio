1b955aa7074740b70db2a8b51c67c05b199f7e8e
Mateo <120265486+M4TE0FR@users.noreply.github.com>
2022-12-31 02:58:37 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach(link => {
+  link.addEventListener("click", event => {
+    event.preventDefault();
+    const targetId = event.target.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+    targetElement.scrollIntoView({ behavior: "smooth" });
+  });
+});
